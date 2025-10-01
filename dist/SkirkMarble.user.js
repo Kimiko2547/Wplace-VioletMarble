f11c8cf363f57bb2c71bde0ad4529422a65af8ed
kimiko2547 <kimiko2547>
2025-10-01 22:19:12 +0100

    Amend header links and reset version.

--- dist/SkirkMarble.user.js
+++ dist/SkirkMarble.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name         Skirk Marble (Experimental Fork)
 // @namespace    https://github.com/Kimiko2547/
-// @version      0.90.4-exp.2
+// @version      0.90.4-exp.1
 // @description  A userscript to automate and/or enhance the user experience on Wplace.live. (Experimental fork by Kimiko2547)
 // @author       Seris0 (fork maintained by Kimiko2547)
 // @license      MPL-2.0
 // @homepageURL  https://github.com/Kimiko2547/Wplace-SkirkMarble-Experimental
 // @supportURL   https://github.com/Kimiko2547/Wplace-SkirkMarble-Experimental/issues
 // @source       https://github.com/Kimiko2547/Wplace-SkirkMarble-Experimental
-// @icon         https://raw.githubusercontent.com/Kimiko2547/Wplace-SkirkMarble-Experimental/main/dist/assets/Favicon.png
-// @updateURL    https://raw.githubusercontent.com/Kimiko2547/Wplace-SkirkMarble-Experimental/main/dist/SkirkMarble.user.js
-// @downloadURL  https://raw.githubusercontent.com/Kimiko2547/Wplace-SkirkMarble-Experimental/main/dist/SkirkMarble.user.js
+// @icon         https://raw.githubusercontent.com/Kimiko2547/Wplace-SkirkMarble-Experimental/feature/testing/dist/assets/Favicon.png
+// @updateURL    https://raw.githubusercontent.com/Kimiko2547/Wplace-SkirkMarble-Experimental/feature/testing/dist/SkirkMarble.user.js
+// @downloadURL  https://raw.githubusercontent.com/Kimiko2547/Wplace-SkirkMarble-Experimental/feature/testing/dist/SkirkMarble.user.js
 // @run-at       document-start
 // @match        *://*.wplace.live/*
 // @grant        GM_getResourceText
@@ -21,7 +21,7 @@
 // @connect      nominatim.openstreetmap.org
 // @connect      wplace.live
 // @connect      wplace.lol
-// @resource     CSS-BM-File https://raw.githubusercontent.com/Kimiko2547/Wplace-SkirkMarble-Experimental/main/dist/SkirkMarble.user.css
+// @resource     CSS-BM-File https://raw.githubusercontent.com/Kimiko2547/Wplace-SkirkMarble-Experimental/feature/testing/dist/SkirkMarble.user.css
 // ==/UserScript==
 
 // Wplace  --> https://wplace.live
